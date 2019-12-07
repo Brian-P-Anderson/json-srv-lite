96dcf59fc67b0267d9f9dfd3b0979d07e4dcea6d
Brian P. Anderson <bpa@notionovus.com>
2019-12-06 20:08:51 -0600

    First try

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var app = express();
 
+const fs = require('fs');
+
+fs.readFile('url.json', (err, data) => {
+    if (err) throw err;
+    let url = JSON.parse(data);
+    console.log(url);
+});
+
+console.log('This is after the read call');
+
 app.get("/url", (req, res, next) => {
-  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+  res.json(url);
  });
 
 app.listen(3000, () => {
